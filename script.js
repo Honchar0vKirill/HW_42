dec3dcbd15b83307451f1dc7eb898e3728c506ea
Hocnchar0vKirill <kirill2021honc@gmail.com>
2023-04-18 12:27:50 +0300

    ...

---
+++ script.js
@@ -0,0 +1,31 @@
+let changeThemeBtn = document.querySelector('#changeBtn');
+const header = document.querySelector('header');
+let headerEl = document.querySelector('#headerItem');
+let mainH1 = document.querySelector('.mainH1');
+let body = document.querySelector('body');
+let heroSection = document.querySelector('#hero-section')
+let contactSection = document.querySelector('#contact-section')
+let contactUs = document.querySelector('#contact-us');
+let heroText = document.querySelector('.hero-text');
+let footer = document.querySelector('footer');
+let footerText = document.querySelector('#ftr-txt')
+let heroHeader = document.querySelector('.hero-header');
+
+const changeTheme = () => {
+    header.style.backgroundColor = 'white';
+    changeThemeBtn.style.backgroundColor = 'black';
+    changeThemeBtn.style.border = 'none';
+    changeThemeBtn.style.color = 'white'
+    headerEl.style.color = 'black';
+    mainH1.style.color = 'black';
+    body.style.backgroundColor = 'white';
+    heroSection.style.backgroundColor = 'white';
+    contactSection.style.backgroundColor = 'white';
+    contactUs.style.color = 'black';
+    heroText.style.color = 'black';
+    footer.style.backgroundColor = 'white';
+    footerText.style.color = 'black';
+    heroHeader.style.color = 'black';
+}
+
+changeThemeBtn.addEventListener('click', changeTheme);
